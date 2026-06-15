5bf8a2901ee012584641ca743780bb7fad1c2152
dcheng093 <chengpeter37093@gmail.com>
2026-06-15 09:10:27 +0800

    stuff

--- Level 2/do_op/do_op.c
+++ Level 2/do_op/do_op.c
@@ -1,26 +1,43 @@
-#include <stdio.h>
+#include "ft_printf.h"
 #include <unistd.h>
-#include <stdlib.h>
 
-int main(int ac, char **av)
+int ft_atoi(const char *str)
+{
+    int i = 0;
+    int res = 0;
+    int sign = 1;
+    while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+        i++;
+    while (str[i] == '+' || str[i] == '-')
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+        res = res * 10 + str[i++] - '0';
+    return res * sign;
+}
+
+int main(int ac, char *av[])
 {
     int res = 0;
     if (ac == 4)
     {
-        int n1 = atoi(av[1]);
-        int n2 = atoi(av[3]);
+        int n1 = ft_atoi(av[1]);
+        int n2 = ft_atoi(av[3]);
         if (av[2][0] == '+')
             res = n1 + n2;
         else if (av[2][0] == '-')
             res = n1 - n2;
-        else if (av[2][0] == '/')
-            res = n1 / n2;
         else if (av[2][0] == '*')
             res = n1 * n2;
+        else if (av[2][0] == '/')
+            res = n1 / n2;
         else if (av[2][0] == '%')
             res = n1 % n2;
-        printf("%d\n", res);
+        ft_printf("%d\n", res);
     }
     else
-        write (1, "\n", 1);
+        write(1, "\n", 1);
 }
--- Level 3/epur_str/epur_str.c
+++ Level 3/epur_str/epur_str.c
@@ -13,16 +13,13 @@ int main(int ac, char **av)
     {
         while (av[1][i] == ' ' || av[1][i] == '\t')
             i++;
-        if (av[1][i] == '\0')
+        if (!av[1][i])
             break;
         if (!first)
             write(1, " ", 1);
         first = 0;
         while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
-        {
-            write(1, &av[1][i], 1);
-            i++;
-        }
+            write(1, &av[1][i++], 1);
     }
     write(1, "\n", 1);
     return (0);
