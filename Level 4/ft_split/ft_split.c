812fbc026217a65e6463d0998358007a8c620bcf
dcheng093 <chengpeter37093@gmail.com>
2026-06-22 13:31:09 +0800

    stuff

--- Level 4/ft_split/ft_split.c
+++ Level 4/ft_split/ft_split.c
@@ -1,24 +1,20 @@
 #include <stdlib.h>
 
-int is_sep(char c)
+int is_delimiter(char c)
 {
-    return (c == ' ' || c == '\t' || c == '\n');
+    return ((c >= 9 && c <= 13) || (c == 32));
 }
 
 int word_count(char *str)
 {
-    int i = 0;
-    int count = 0;
-
+    int count = 0, i = 0;
     while (str[i])
     {
-        while (str[i] && is_sep(str[i]))
+        while (str[i] && is_delimiter(str[i]))
             i++;
-
         if (str[i])
             count++;
-
-        while (str[i] && !is_sep(str[i]))
+        while (str[i] && !is_delimiter(str[i]))
             i++;
     }
     return count;
@@ -26,40 +22,28 @@ int word_count(char *str)
 
 char **ft_split(char *str)
 {
-    int i = 0;
-    int j;
-    int k = 0;
-    int words = word_count(str);
-    char **res;
-
-    res = malloc(sizeof(char *) * (words + 1));
+    int i = 0, k = 0, words = word_count(str);
+    char **res = malloc(sizeof(char *) * (words + 1));
     if (!res)
         return NULL;
-
     while (str[i])
     {
-        while (str[i] && is_sep(str[i]))
+        while (str[i] && is_delimiter(str[i]))
             i++;
-
         if (str[i])
         {
-            j = 0;
-            int start = i;
-
-            while (str[i] && !is_sep(str[i]))
+            int j = 0, start = i;
+            while (str[i] && !is_delimiter(str[i]))
                 i++;
-
             res[k] = malloc(sizeof(char) * (i - start + 1));
             if (!res[k])
                 return NULL;
-
             while (start < i)
                 res[k][j++] = str[start++];
-
             res[k][j] = '\0';
-            k++;
+            k++; 
         }
     }
-    res[k] = NULL;
+    res[k] = '\0';
     return res;
 }
